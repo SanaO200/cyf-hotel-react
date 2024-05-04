ed3cda3ffa491c858649d1013ff1eba97e929512
P.F. <pavel.filkovskiy@gmail.com>
2024-05-04 11:39:49 +0100

    add routers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,18 +4,41 @@ import TouristInfoCards from './components/TouristInfoCards'
 import Restaurant from './components/Restaurant'
 import Footer from './components/Footer'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import './App.css'
 
-const App = () => {
-  const contacts = ['123 Fake Street, London, E1 4UD', 'hello@fakehotel.com', '0123 456789']
+const contacts = ['123 Fake Street, London, E1 4UD', 'hello@fakehotel.com', '0123 456789']
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <>
+        <Header />
+        <TouristInfoCards />
+        <Bookings />
+        <Restaurant />
+        <Footer contacts={contacts} />
+      </>
+    ),
+  },
+  {
+    path: '/about',
+    element: (
+      <>
+        <Header />
+        <h1>About Us</h1>
+        <Footer contacts={contacts} />
+      </>
+    ),
+  },
+])
+
+const App = () => {
   return (
     <div className='App'>
-      <Header />
-      <TouristInfoCards />
-      <Bookings />
-      <Restaurant />
-      <Footer contacts={contacts} />
+      <RouterProvider router={router} />
     </div>
   )
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
+  return (
+    <div>
+      <header className='App-header'>
+        <Link to='/'>CYF Hotel</Link>
+        <Link to='/about'>About Us</Link>
+      </header>
+      <img
+        src='https://cf.bstatic.com/xdata/images/hotel/max1024x768/301849517.jpg?k=0cfebc11abf146f5f7c59dff880fae95df61c9e8368691d62e0bda769d2c0ce4&o=&hp=1'
+        alt='header pic'
+        className='App-logo'
+      />
+    </div>
+  )
+}
 
-	return (
-<div>
-	<header className='App-header'>CYF Hotel</header>
-		<img src ='https://cf.bstatic.com/xdata/images/hotel/max1024x768/301849517.jpg?k=0cfebc11abf146f5f7c59dff880fae95df61c9e8368691d62e0bda769d2c0ce4&o=&hp=1' alt="header pic" className='App-logo'/>
-</div>
-
-	)
-  }
-
-  export default Header
+export default Header
